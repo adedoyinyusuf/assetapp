f79f1d5088adc0a46e83ca76e5b677f5c57d4c44
Adedoyin Yusuf <98955115+nationalpopulationhq@users.noreply.github.com>
2025-08-30 06:48:30 +0100

    30august

--- next-build/server/server-reference-manifest.js
+++ next-build/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"2rY8aK8J2v5Fa6cjYWgpufnpkkoPz5oz4PIhhpUO7AI=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"wjoBw/37UfF1p5GbMkxHHZAQEdLQ2f2Te5ykr/yJ678=\"\n}"
